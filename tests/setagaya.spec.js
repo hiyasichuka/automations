ec6411ea545768aaf2a990d88b3ab448212be09e
takahashi-kei <takahashi-kei@dmm.com>
2025-01-19 11:34:48 +0900

    検索テストを更新し、新しいタブでの操作を追加。エラーメッセージの確認とスクリーンショットの取得を実装。

--- tests/setagaya.spec.js
+++ tests/setagaya.spec.js
@@ -1,15 +1,35 @@
 import { test } from '@playwright/test';
 
-test('検索', async ({ page }) => {
+test('検索', async ({ context, page }) => {
 
-  await page.goto('https://jhomes.to-kousya.or.jp/search/jkknet/service/akiyaJyoukenStartInit');
-  await page.check('input[type="checkbox"][value="世田谷区"]');
-  await page.click('input[type="button"][value="検索する"]');
+    const newTabPromise = context.waitForEvent("page");
 
-  const noResultsMessage = page.locator('.error');
-  if (await noResultsMessage.count() > 0) {
-    console.log('見つかりませんでした');
-  } else {
-    throw new Error('見つかりました');
-  }
+    await page.goto('https://jhomes.to-kousya.or.jp/search/jkknet/service/akiyaJyoukenStartInit');
+    // 「こちら」のリンクが描画されるまで待機
+    await page.waitForSelector('a[onclick*="submitNext"]');
+
+    await page.click('a[onclick*="submitNext"]')
+    
+    const newTab = await newTabPromise;
+    await newTab.waitForLoadState("domcontentloaded");
+
+    // チェックボックス
+    await newTab.check('input[type="checkbox"][name="akiyaInitRM.akiyaRefM.checks"][value="12"]');
+    await newTab.waitForSelector('a[onclick*="submitPage"]');
+    await newTab.waitForSelector('a[onclick*="submitPage(\'akiyaJyoukenRef\')"]', { state: 'visible' });
+    await newTab.click('a[onclick*="submitPage(\'akiyaJyoukenRef\')"]');
+    await newTab.waitForSelector('li.error', { state: 'visible',  timeout: 5000  });
+    const errorExists = await newTab.locator('li.error').count() > 0;
+
+    if (errorExists) {
+    console.log('li.error が表示されています。');
+    } else {
+    console.log('li.error は表示されていません。');
+    }
+
+    // スクリーンショットを取得
+    await newTab.screenshot({ path: 'screenshot.png', fullPage: true });
+
+    console.log('スクリーンショットを取得しました: screenshot.png');
+
 });
