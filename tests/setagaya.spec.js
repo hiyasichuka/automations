241a31c2827f36591ecf043858e770e1dedf5d4c
takahashi-kei <takahashi-kei@dmm.com>
2025-01-19 11:36:10 +0900

    検索テストを簡素化し、エラーメッセージの確認を改善。スクリーンショット取得の処理を整理。

--- tests/setagaya.spec.js
+++ tests/setagaya.spec.js
@@ -1,35 +1,15 @@
 import { test } from '@playwright/test';
 
 test('検索', async ({ context, page }) => {
-
-    const newTabPromise = context.waitForEvent("page");
-
-    await page.goto('https://jhomes.to-kousya.or.jp/search/jkknet/service/akiyaJyoukenStartInit');
-    // 「こちら」のリンクが描画されるまで待機
-    await page.waitForSelector('a[onclick*="submitNext"]');
-
-    await page.click('a[onclick*="submitNext"]')
-    
-    const newTab = await newTabPromise;
-    await newTab.waitForLoadState("domcontentloaded");
-
-    // チェックボックス
-    await newTab.check('input[type="checkbox"][name="akiyaInitRM.akiyaRefM.checks"][value="12"]');
-    await newTab.waitForSelector('a[onclick*="submitPage"]');
-    await newTab.waitForSelector('a[onclick*="submitPage(\'akiyaJyoukenRef\')"]', { state: 'visible' });
-    await newTab.click('a[onclick*="submitPage(\'akiyaJyoukenRef\')"]');
-    await newTab.waitForSelector('li.error', { state: 'visible',  timeout: 5000  });
-    const errorExists = await newTab.locator('li.error').count() > 0;
-
-    if (errorExists) {
-    console.log('li.error が表示されています。');
-    } else {
-    console.log('li.error は表示されていません。');
-    }
-
-    // スクリーンショットを取得
-    await newTab.screenshot({ path: 'screenshot.png', fullPage: true });
-
-    console.log('スクリーンショットを取得しました: screenshot.png');
-
+  const newTabPromise = context.waitForEvent("page");
+  await page.goto('https://jhomes.to-kousya.or.jp/search/jkknet/service/akiyaJyoukenStartInit');
+  await page.click('a[onclick*="submitNext"]');
+  const newTab = await newTabPromise;
+  await newTab.waitForLoadState("domcontentloaded");
+  await newTab.check('input[type="checkbox"][name="akiyaInitRM.akiyaRefM.checks"][value="12"]');
+  await newTab.click('a[onclick*="submitPage(\'akiyaJyoukenRef\')"]');
+  const errorExists = await newTab.locator('li.error').isVisible({ timeout: 5000 });
+  console.log(errorExists ? 'li.error が表示されています。' : 'li.error は表示されていません。');
+  await newTab.screenshot({ path: 'screenshot.png', fullPage: true });
+  console.log('スクリーンショットを取得しました: screenshot.png');
 });
