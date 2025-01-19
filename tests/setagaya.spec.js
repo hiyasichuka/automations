541face43b6d1b6af1b4c995d7b8501d2860a463
takahashi-kei <takahashi-kei@dmm.com>
2025-01-19 12:03:19 +0900

    検索テストからチェックボックスの確認とスクリーンショット取得の処理をコメントアウト

--- tests/setagaya.spec.js
+++ tests/setagaya.spec.js
@@ -6,11 +6,11 @@ test('検索', async ({ context, page }) => {
   await page.click('a[onclick*="submitNext"]');
   const newTab = await newTabPromise;
   await newTab.waitForLoadState("domcontentloaded");
-  await newTab.check('input[type="checkbox"][name="akiyaInitRM.akiyaRefM.checks"][value="12"]');
+  // await newTab.check('input[type="checkbox"][name="akiyaInitRM.akiyaRefM.checks"][value="12"]');
   await newTab.click('a[onclick*="submitPage(\'akiyaJyoukenRef\')"]');
   await newTab.waitForSelector('li.error', { state: 'visible', timeout: 3000 });
   const errorExists = await newTab.locator('li.error').isVisible();
   console.log(errorExists ? 'li.error が表示されています。' : 'li.error は表示されていません。');
-  await newTab.screenshot({ path: 'screenshot.png', fullPage: true });
-  console.log('スクリーンショットを取得しました: screenshot.png');
+  // await newTab.screenshot({ path: 'screenshot.png', fullPage: true });
+  // console.log('スクリーンショットを取得しました: screenshot.png');
 });
