5cb5bd4a89d9222fcf709cc0c1bf0c4cf2b4318c
takahashi-kei <takahashi-kei@dmm.com>
2025-01-19 12:27:53 +0900

    Playwrightテストのスケジュールを修正し、チェックボックスの確認処理を追加

--- tests/setagaya.spec.js
+++ tests/setagaya.spec.js
@@ -6,11 +6,18 @@ test('検索', async ({ context, page }) => {
   await page.click('a[onclick*="submitNext"]');
   const newTab = await newTabPromise;
   await newTab.waitForLoadState("domcontentloaded");
+
+  // ward
   await newTab.check('input[type="checkbox"][name="akiyaInitRM.akiyaRefM.checks"][value="12"]');
+  // house layout
+  await newTab.check('input[type="checkbox"][name="akiyaInitRM.akiyaRefM.madoris"][value="2"]');
+  await newTab.check('input[type="checkbox"][name="akiyaInitRM.akiyaRefM.madoris"][value="3"]');
+
   await newTab.click('a[onclick*="submitPage(\'akiyaJyoukenRef\')"]');
   await newTab.waitForSelector('li.error', { state: 'visible', timeout: 3000 });
   const errorExists = await newTab.locator('li.error').isVisible();
-  console.log(errorExists ? 'li.error が表示されています。' : 'li.error は表示されていません。');
+  console.log(errorExists ? '見つからず' : '見つかった');
+
   // await newTab.screenshot({ path: 'screenshot.png', fullPage: true });
   // console.log('スクリーンショットを取得しました: screenshot.png');
 });
