4a8f2d9d623e41d326fa932efd198d6d50b72e5c
takahashi-kei <takahashi-kei@dmm.com>
2025-01-19 12:06:32 +0900

    チェックボックスの確認処理を有効化

--- tests/setagaya.spec.js
+++ tests/setagaya.spec.js
@@ -6,7 +6,7 @@ test('検索', async ({ context, page }) => {
   await page.click('a[onclick*="submitNext"]');
   const newTab = await newTabPromise;
   await newTab.waitForLoadState("domcontentloaded");
-  // await newTab.check('input[type="checkbox"][name="akiyaInitRM.akiyaRefM.checks"][value="12"]');
+  await newTab.check('input[type="checkbox"][name="akiyaInitRM.akiyaRefM.checks"][value="12"]');
   await newTab.click('a[onclick*="submitPage(\'akiyaJyoukenRef\')"]');
   await newTab.waitForSelector('li.error', { state: 'visible', timeout: 3000 });
   const errorExists = await newTab.locator('li.error').isVisible();
