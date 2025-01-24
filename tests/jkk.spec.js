593de8df2ff4f48899265a3e6f695b287e59e40c
hiyasichuka <hiyasichukahajimemasita@gmail.com>
2025-01-25 07:37:56 +0900

    Refactor Playwright test script for consistency and readability

--- tests/jkk.spec.js
+++ tests/jkk.spec.js
@@ -1,23 +1,28 @@
-import { test } from '@playwright/test';
+import { test } from "@playwright/test";
 
-test('検索', async ({ context, page }) => {
+test("検索", async ({ context, page }) => {
   const newTabPromise = context.waitForEvent("page");
-  await page.goto('https://jhomes.to-kousya.or.jp/search/jkknet/service/akiyaJyoukenStartInit');
+  await page.goto(
+    "https://jhomes.to-kousya.or.jp/search/jkknet/service/akiyaJyoukenStartInit"
+  );
   await page.click('a[onclick*="submitNext"]');
   const newTab = await newTabPromise;
   await newTab.waitForLoadState("domcontentloaded");
 
   // ward
-  await newTab.check('input[type="checkbox"][name="akiyaInitRM.akiyaRefM.checks"][value="12"]');
+  await newTab.check(
+    'input[type="checkbox"][name="akiyaInitRM.akiyaRefM.checks"][value="12"]'
+  );
   // house layout
-  await newTab.check('input[type="checkbox"][name="akiyaInitRM.akiyaRefM.madoris"][value="2"]');
-  await newTab.check('input[type="checkbox"][name="akiyaInitRM.akiyaRefM.madoris"][value="3"]');
+  await newTab.check(
+    'input[type="checkbox"][name="akiyaInitRM.akiyaRefM.madoris"][value="2"]'
+  );
+  await newTab.check(
+    'input[type="checkbox"][name="akiyaInitRM.akiyaRefM.madoris"][value="3"]'
+  );
 
-  await newTab.click('a[onclick*="submitPage(\'akiyaJyoukenRef\')"]');
-  await newTab.waitForSelector('li.error', { state: 'visible', timeout: 3000 });
-  const errorExists = await newTab.locator('li.error').isVisible();
-  console.log(errorExists ? '見つからず' : '見つかった');
-
-  // await newTab.screenshot({ path: 'screenshot.png', fullPage: true });
-  // console.log('スクリーンショットを取得しました: screenshot.png');
+  await newTab.click("a[onclick*=\"submitPage('akiyaJyoukenRef')\"]");
+  await newTab.waitForSelector("li.error", { state: "visible", timeout: 3000 });
+  const errorExists = await newTab.locator("li.error").isVisible();
+  console.log(errorExists ? "見つからず" : "見つかった");
 });
