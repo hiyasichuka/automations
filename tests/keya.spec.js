cb614a51bd309a6a768ec9d91a63ab26ebca5410
takahashi-kei <takahashi-kei@dmm.com>
2025-02-15 20:31:00 +0900

    テストのロジックを修正

--- tests/keya.spec.js
+++ tests/keya.spec.js
@@ -4,9 +4,10 @@ const baseUrl = "https://setagaya.keyakinet.net";
 const screenshotPath = "./screenshots/keya.png";
 const symbols = ["〇", "△"];
 const locations = [
-  "羽根木公園",
+  // "羽根木公園",
   "用賀中学校",
   "桜丘中学校",
+  "芦花中学校",
 ];
 const conditions = ["土", /^日$/, "祝", "ヶ月"];
 
@@ -20,14 +21,13 @@ test("test", async ({ page }) => {
   };
 
   const checkSymbols = async (tag) => {
-    console.log(`Checking ${tag}`);
     await page.waitForSelector("tbody"); // tbodyが表示されるまで待機
     const bodyTexts = await page.locator("tbody").allInnerTexts();
     const combinedText = bodyTexts.join("\n");
     for (const symbol of symbols) {
       if (combinedText.includes(symbol)) {
         await page.screenshot({ path: screenshotPath, fullPage: true })
-        throw new Error(`Tag: ${tag} ,The text contains symbol: ${symbol}`);
+        throw new Error(`${tag} contains:${symbol}`);
       }
     }
     return combinedText;
@@ -61,15 +61,28 @@ test("test", async ({ page }) => {
 
     await navigateAndClick("link", "前に戻る");
     await page.getByRole("cell", { name: location }).locator("label").click();
-
   }
 
-  // const date = new Date();
-  // const day = date.getDate();
-  // await page.getByPlaceholder('/2/13').click();
-  // await page.getByTitle('').click();
-  // await page.getByRole('link', { name: day, exact: true }).click();
-  // await navigateAndClick('button', '表示');
-  // await checkSymbols("1ヶ月後");
-  // console.log("空きコートなし");
+  firstFlag = true;
+  for (const location of locations) {
+    await page.getByRole("cell", { name: location }).locator("label").click();
+    await navigateAndClick("link", "次へ進む");
+
+    if(firstFlag){
+      const date = new Date();
+      const day = date.getDate();
+      await page.getByPlaceholder('/2/13').click();
+      await page.getByTitle('').click();
+      await page.getByRole('link', { name: day, exact: true }).click();
+      firstFlag = false
+    }
+
+    await navigateAndClick("button", "表示");
+    await checkSymbols("1ヶ月後: " + location);
+
+    await navigateAndClick("link", "前に戻る");
+    await page.getByRole("cell", { name: location }).locator("label").click();
+  }
+
+  console.log("空きコートなし");
 });
