5a48f56b634d2922ab051d6557e0c93b55ad7c89
hiyasichuka <hiyasichukahajimemasita@gmail.com>
2025-07-26 08:00:26 +0900

    update cost

--- tests/jkk.spec.js
+++ tests/jkk.spec.js
@@ -31,7 +31,7 @@ test("検索", async ({ context, page }) => {
   );
 
   // price
-  await newTab.locator('select[name="akiyaInitRM\\.akiyaRefM\\.yachinFrom"]').selectOption('160000');
+  await newTab.locator('select[name="akiyaInitRM\\.akiyaRefM\\.yachinFrom"]').selectOption('140000');
 
   await newTab.click("a[onclick*=\"submitPage('akiyaJyoukenRef')\"]");
   await newTab.waitForSelector("li.error", { state: "visible", timeout: 3000 });
