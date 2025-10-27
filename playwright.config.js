3a6e965c0ac4bcd33df539d6a6b118f227c54f2e
takahashi-kei <takahashi-kei@dmm.com>
2025-10-28 08:18:17 +0900

    Update viewport settings in Playwright config and change script permissions

--- playwright.config.js
+++ playwright.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     {
       name: 'chromium',
       use: {
-        viewport: { width: 1920, height: 1080 },
+        viewport: { width: 1080, height: 720 },
       },
     },
   ],
